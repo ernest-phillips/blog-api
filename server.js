53639f4a4bf765c14cbaf45062c061e00958e4a9
ernest-phillips <ernestlphillips@gmail.com>
2018-10-19 18:40:34 -0700

    completed app?

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+const blogPostsRouter = require('./blogPostsRouter');
+
+// http layer
+app.use(morgan('common'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/blogPostsRouter', blogPostsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
