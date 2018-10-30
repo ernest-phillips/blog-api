b8983de370df948b097c5967e593bdd7bb0ff04f
ernest-phillips <ernestlphillips@gmail.com>
2018-10-30 11:11:23 -0700

    added server.js

--- test/test-server.js
+++ test/test-server.js
@@ -1,11 +1,11 @@
 const chai = require('chai');
-const chaiHttp = reqire('chai-http');
+const chaiHttp = require('chai-http');
 
 const {app, runServer, closeServer} = require('../server')
 
 const should = chai.should();
 
-describe('Blog', function(){
+describe('Blog Post', function(){
 	before(function() {
 		return runServer();
 	});
@@ -16,7 +16,8 @@ after(function() {
 });
 
 it('should list blog posts on GET', function() {
-	return chai.request(app)
+	return chai
+	.request(app)
 	.get('/blog')
 	.then(function (res) {
 		res.should.have.status(400);
@@ -27,24 +28,38 @@ it('should list blog posts on GET', function() {
 
 		res.body.forEach(function (item) {
 			item.should.be.a('object');
-			item.should.include.keys('title','content','author', 'publishDate');
+			item.should.include.keys
+			(
+				'id',
+				'title',
+				'content',
+				'author', 
+				'publishDate'
+				);
 		});
 	});
 }); //end GET
 
 it('should add a blog post on POST', function () {
 	const newBlogPost = {
-		title: 'post title', content: 'add some content.',author:'Sally Student', publishDate: "10-20-2018" 
+		title: 'post title', 
+		content: 'add some content.',
+		author:'Sally Student' 
 	};
+	const expectedKeys = ["id", "publishDate"].concat(Object.keys(newBlogPost));
+
+	return chai
+	.request(app)
 	.post('/blog')
 	.send(newBlogPost)
 	.then(function (res) {
 		res.should.have.status(201);
 		res.should.be.json;
 		res.body.should.be.a('object');
-		res.body.should.include.keys('title','content','author', 'publishDate');
-		
-		res.body.newBlogPost.should.include.members(newBlogPost)
+		res.body.should.include.keys('id','title','content','author', 'publishDate');
+		res.body.should.equal(newBlogPost.content);
+		res.body.should.equal(newBlogPost.title);
+		res.body.author.should.equal(newBlogPost.author);
 		
 	});
 });
